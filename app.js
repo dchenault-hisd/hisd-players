e46abe5ac6726fcb6b4f1ef08ad64e4fe876dc34
dchenault-hisd <dchenault@hendersonisd.org>
2026-04-29 12:38:22 -0500

    Update app.js

--- app.js
+++ app.js
@@ -391,6 +391,8 @@ document.addEventListener("DOMContentLoaded", async () => {
   if (document.body.dataset.page === "home") renderHome(data);
   if (document.body.dataset.page === "directory") renderDirectory(data);
   if (document.body.dataset.page === "profile") renderProfile(data);
+ if (document.body.dataset.page === "profile-pdf") {
+  renderProfilePDF(data);
 });
 
 console.log("HISD Recruiting live-data announcements v7 loaded");
